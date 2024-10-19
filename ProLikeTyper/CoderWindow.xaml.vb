53818e5cc0950dfff2f691f8064c9a95d9225852
PC-DOS <suicunexp@outlook.com>
2024-10-20 01:51:35 +0800

    Initial commit

---
+++ ProLikeTyper/CoderWindow.xaml.vb
@@ -0,0 +1,117 @@
+﻿Imports System.Reflection
+Imports System.IO
+Imports System.Text
+Imports System.Windows.Threading
+Public Class CoderWindow
+    'Timer object for "coding"
+    Dim CoderTimer As New DispatcherTimer()
+    Dim CursorTimer As New DispatcherTimer()
+
+    'Code source
+    Const InternalCodeSource As String = "CoderText.txt"
+    Const ExternalCodeSource As String = "CoderText.txt"
+    Dim CodeSourceList As New List(Of String)
+    Dim CodeSourceLinePointer As Integer
+    Dim CodeSourceColumnPointer As Integer
+
+    'Display text of code
+    Dim CodeText As String = ""
+    Dim CodeTextDisplay As String = ""
+
+    'Console cursor
+    Dim IsConsoleCursorVisible As Boolean = True
+    Const ConsoleCursorFull As String = "▮"
+    Const ConsoleCursorEmpty As String = "▯"
+    Private Function GenerateRandomCodeLineNumber() As Integer
+        Dim LinePointerGenerator As New Random
+        Return LinePointerGenerator.Next() Mod CodeSourceList.Count
+    End Function
+    Private Sub CoderWindow_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
+        'Try loading code from external code file
+        Dim ExternalCodeFilePath As String = AppDomain.CurrentDomain.SetupInformation.ApplicationBase & "\" & ExternalCodeSource
+        If File.Exists(ExternalCodeFilePath) Then
+            Dim ExternalCodeFileStream As StreamReader = File.OpenText(ExternalCodeFilePath)
+            While Not ExternalCodeFileStream.EndOfStream
+                CodeSourceList.Add(ExternalCodeFileStream.ReadLine())
+            End While
+            ExternalCodeFileStream.Close()
+        End If
+
+        'If no external code file available, or code file is empty, fall back to internal code source
+        If CodeSourceList.Count = 0 Then
+            Dim InternalCodeFilePath As String = MethodBase.GetCurrentMethod().DeclaringType.Namespace & "." & InternalCodeSource
+            Dim InternalCodeFileRawStream As Stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(InternalCodeFilePath)
+            Dim InternalCodeFileStream As New StreamReader(InternalCodeFileRawStream)
+            While Not InternalCodeFileStream.EndOfStream
+                CodeSourceList.Add(InternalCodeFileStream.ReadLine())
+            End While
+            InternalCodeFileStream.Close()
+        End If
+
+        'Initialize code pointers
+        CodeSourceLinePointer = GenerateRandomCodeLineNumber()
+        CodeSourceColumnPointer = 0
+
+        'Initialize timer
+        AddHandler CoderTimer.Tick, AddressOf CoderTimer_Tick
+        CoderTimer.Interval = TimeSpan.FromMilliseconds(25)
+        CoderTimer.Start()
+        AddHandler CursorTimer.Tick, AddressOf CursorTimer_Tick
+        CursorTimer.Interval = TimeSpan.FromMilliseconds(500)
+        CursorTimer.Start()
+    End Sub
+
+    Private Sub CoderTimer_Tick()
+        ''Testing: generate a random text from ASCII table
+        'Dim CharAscii As Integer = 0
+        'Dim CharGenerator As New Random
+        'CharAscii = CharGenerator.Next() Mod 97 + 32
+        'If CharAscii <> 128 Then
+        '    CodeText = CodeText & Chr(CharAscii)
+        'Else
+        '    CodeText = CodeText & vbCrLf
+        'End If
+
+        'Load next character from code source
+        If CodeSourceList(CodeSourceLinePointer).Length > 0 Then
+            CodeText = CodeText & CodeSourceList(CodeSourceLinePointer)(CodeSourceColumnPointer)
+        End If
+        'Update line & column pointers
+        CodeSourceColumnPointer += 1
+        If CodeSourceColumnPointer >= CodeSourceList(CodeSourceLinePointer).Length Then
+            'Add Enter key
+            CodeText = CodeText & vbCrLf
+
+            'Change to new row
+            CodeSourceColumnPointer = 0
+            CodeSourceLinePointer += 1
+            If CodeSourceLinePointer >= CodeSourceList.Count Then
+                CodeSourceLinePointer = GenerateRandomCodeLineNumber()
+            End If
+        End If
+
+        'Concat cursor
+        If IsConsoleCursorVisible Then
+            CodeTextDisplay = CodeText & ConsoleCursorFull
+        Else
+            CodeTextDisplay = CodeText & ConsoleCursorEmpty
+        End If
+
+        'Update display
+        lblCode.Text = CodeTextDisplay
+        scrCodeContainer.ScrollToEnd()
+    End Sub
+
+    Private Sub CursorTimer_Tick()
+        'Flip console's cursor status
+        IsConsoleCursorVisible = Not IsConsoleCursorVisible
+        If IsConsoleCursorVisible Then
+            CodeTextDisplay = CodeText & ConsoleCursorFull
+        Else
+            CodeTextDisplay = CodeText & ConsoleCursorEmpty
+        End If
+
+        'Update display
+        'lblCode.Text = CodeTextDisplay
+    End Sub
+End Class
---
+++ ProLikeTyper/MainWindow.xaml.vb
@@ -0,0 +1,90 @@
+﻿Class MainWindow
+    Const CommandNameDefault As String = "!UnnamedCommand!"
+    Const CommandInternalNameDefault As String = "!NOCMDNAME!"
+    Const CommandDescriptionNoSelection As String = "Double-click on an item to execute it"
+    Const CommandDescriptionDefault As String = "No available descriptions for this command"
+    Private Structure CommandItem
+        Dim Name As String
+        Dim InternalName As String
+        Dim Description As String
+    End Structure
+    Private Function ParseSelectedCommand(SelectedCommand As ListBoxItem) As CommandItem
+        Dim CommandData As CommandItem
+        'Split selected item tag by "_", tag is organized in following structure:
+        'CommandInternalName_Description
+        Dim SelectedCommandTags As String() = Split(SelectedCommand.Tag.ToString(), "_")
+        Dim SelectedCommandDescription As String = ""
+        Dim SelectedCommandName As String = SelectedCommand.Content.ToString().Trim()
+        Dim SelectedCommandInternalName As String = SelectedCommandTags(0).Trim()
+
+        'Restore potential underline(s) in description
+        If UBound(SelectedCommandTags) >= 1 Then
+            Dim i As Integer
+            For i = 1 To UBound(SelectedCommandTags)
+                SelectedCommandDescription = SelectedCommandDescription & SelectedCommandTags(i)
+                If i <> UBound(SelectedCommandTags) Then
+                    SelectedCommandDescription = SelectedCommandDescription & "_"
+                End If
+            Next
+        End If
+
+        'Apply default data if necessary
+        If SelectedCommandName.Length = 0 Then
+            SelectedCommandName = CommandNameDefault
+        End If
+        If SelectedCommandInternalName.Length = 0 Then
+            SelectedCommandInternalName = CommandInternalNameDefault
+        End If
+        SelectedCommandDescription = SelectedCommandDescription.Trim()
+        If SelectedCommandDescription.Length = 0 Then
+            SelectedCommandDescription = CommandDescriptionDefault
+        End If
+
+        'Return data
+        With CommandData
+            .Name = SelectedCommandName
+            .InternalName = SelectedCommandInternalName.ToUpper
+            .Description = SelectedCommandDescription
+        End With
+        Return CommandData
+    End Function
+
+    Private Sub lstCommands_MouseDoubleClick(sender As Object, e As MouseButtonEventArgs) Handles lstCommands.MouseDoubleClick
+        If lstCommands.SelectedIndex >= 0 Then
+            'Parse selected item
+            Dim SelectedCommandData As CommandItem = ParseSelectedCommand(lstCommands.SelectedItem)
+
+            'Display command description
+            lblCommandDescription.Text = SelectedCommandData.Name & " (" & SelectedCommandData.InternalName & ")" & vbCrLf & SelectedCommandData.Description
+
+            'Execute command
+            Select Case SelectedCommandData.InternalName
+                Case "CODER"
+                    Dim CoderWindowInstance As New CoderWindow
+                    CoderWindowInstance.Show()
+                Case "NETSCAN"
+
+                Case "SYSMON"
+
+                Case "EXIT"
+                    Application.Current.Shutdown()
+                Case Else
+                    'Do nothing currently
+            End Select
+        Else
+            lblCommandDescription.Text = CommandDescriptionNoSelection
+        End If
+    End Sub
+
+    Private Sub lstCommands_SelectionChanged(sender As Object, e As SelectionChangedEventArgs) Handles lstCommands.SelectionChanged
+        If lstCommands.SelectedIndex >= 0 Then
+            'Parse selected item
+            Dim SelectedCommandData As CommandItem = ParseSelectedCommand(lstCommands.SelectedItem)
+
+            'Display command description
+            lblCommandDescription.Text = SelectedCommandData.Name & " (" & SelectedCommandData.InternalName & ")" & vbCrLf & SelectedCommandData.Description
+        Else
+            lblCommandDescription.Text = CommandDescriptionNoSelection
+        End If
+    End Sub
+End Class
